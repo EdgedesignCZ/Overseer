7cac68b9e27834fbe3047b74fe48c5d4d05db8b3
Zdeněk Drahoš <drahoszdenek@gmail.com>
2014-12-02 07:39:25 +0100

    Fix ignoring multiple expressions

--- src/Grep.php
+++ src/Grep.php
@@ -4,10 +4,26 @@ namespace Edge\Overseer;
 
 class Grep
 {
+    private $ignoredPatterns;
 
-    public function invertGrep($pattern, $text)
+    public function __construct(array $ignoredPatterns)
+    {
+        $this->ignoredPatterns = $ignoredPatterns;
+    }
+
+    public function filterOutIgnoredLines($text)
+    {
+        return array_reduce(
+            $this->ignoredPatterns,
+            function ($filteredText, $regexp) {
+                return $this->invertGrep($regexp, $filteredText);
+            },
+            $text
+        );
+    }
+
+    private function invertGrep($pattern, $text)
     {
         return implode("\n", preg_grep($pattern, explode("\n", $text), PREG_GREP_INVERT));
     }
-
 }
